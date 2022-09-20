c0c28d80c578e575ee79f9a4fdcd19f5157d67df
Salih Yılmaz <salih_yil@live.com>
2022-09-20 12:43:31 +0300

    04MVCYapısı-SeparationOfConcerns, ModelViewControl

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -15,20 +16,8 @@ app.set('view engine', 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
-//get'den sonraki ilk parametre root http://localhost:3000/ girince gelecek ekran
-//middleware: request ve response arasında kendi özel işlemlerimizin yapılmasını istediğimiz middleware kullanırız.
-//nodemon: sunucudaki değişiklikleri otomaik anlıcak ve bizde sunucuyu zırt bırt yeniden başlatmak kurtulcaz.
-app.get('/', (request, response) => {
-    response.render('index');
-});
-
-app.get('/about', (request, response) => {
-    response.render('about');
-});
-
-app.get('/blog', (request, response) => {
-    response.render('blog');
-});
+//routes
+app.use(pageRoute);
 
 app.listen(port, () => {
     console.log(`Application  running on port: ${port}`);
